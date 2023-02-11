8c7e331a2f87699525a8b29af8300b58fffd3ab2
Шумилов Артем Михайлович <tema-969@yandex.ru>
2023-02-11 21:55:23 +0300

    решена 2 задача + доработан метод заполнения массива

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -3,19 +3,19 @@
 // [345, 897, 568, 234] -> 2
 var random = new Random();
 int lenRand = random.Next(4, 10);
-int[] arr = new int[lenRand];
+int[] arrThree = new int[lenRand];
 
-FillArray(arr);
-PrintArray(arr);
+FillArray(arrThree,100,1000);
+PrintArray(arrThree);
 System.Console.WriteLine();
-CountEvenNumber(arr);
+CountEvenNumber(arrThree);
 
-void FillArray (int[] numb){
-    for (int i = 0; i < numb.Length; i++)
-    {
-        numb[i] = new Random().Next(100, 1000);
-    }
-}
+// void FillThreeArray (int[] numb){
+//     for (int i = 0; i < numb.Length; i++)
+//     {
+//         numb[i] = new Random().Next(100, 1000);
+//     }
+// }
 
 void PrintArray (int[] el){
     for (int i = 0; i < el.Length; i++)
@@ -41,7 +41,32 @@ void CountEvenNumber (int[] number){
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] arr = new int[lenRand];
 
+FillArray(arr,-100,100);
+PrintArray(arr);
+System.Console.WriteLine();
+SumOddNumber(arr);
+
+void FillArray (int[] numb, int minValue, int maxValue){
+    for (int i = 0; i < numb.Length; i++)
+    {
+        numb[i] = new Random().Next(minValue, maxValue);
+    }
+}
+
+void SumOddNumber (int[] number){
+    int sum = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        if(i%2!=0){
+            sum = sum + number[i];
+        }
+    }
+    
+    System.Console.WriteLine("Сумма чисел нечетных позицый массива" + " = " + sum);
+    
+}
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
