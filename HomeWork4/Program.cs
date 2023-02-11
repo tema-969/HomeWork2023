0a6e0799c0377ba052d125dd6ffc43998fbc08c8
Шумилов Артем Михайлович <tema-969@yandex.ru>
2023-02-11 18:10:58 +0300

    решена 3 задача

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -54,5 +54,22 @@ else {
 return sumOfDigits;
 }
 
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void RandomMassive ()
+{
+int[] arr = new int[8];
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(-100, 100);
+    System.Console.Write(arr[i]+" ");
+}
+
+}
+
 Exponentiation();
 SumOfDigits();
+RandomMassive();
