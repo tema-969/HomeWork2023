e683a08a7b1517ac8f963e8881f099d50f5a2af3
Шумилов Артем Михайлович <tema-969@yandex.ru>
2023-02-15 22:24:50 +0300

    Решено 2 задание

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -6,40 +6,38 @@
 // string strnumber = Console.ReadLine();
 // CountNumber(strnumber);
 
-void CountNumber (string strnumbers ){
-string[] strnum = strnumber.Split(' ');
-int count = 0;
-foreach (var item in strnum)
-{
-    int num = Convert.ToInt32(item);
-    if (num >0) {
-        count++;
-    }
-}
-System.Console.WriteLine("Количество чисел больше 0 = "+count);
-}
+// void CountNumber (string strnumbers ){
+// string[] strnum = strnumber.Split(' ');
+// int count = 0;
+// foreach (var item in strnum)
+// {
+//     int num = Convert.ToInt32(item);
+//     if (num >0) {
+//         count++;
+//     }
+// }
+// System.Console.WriteLine("Количество чисел больше 0 = "+count);
+// }
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Intersection();
 
+
+void Intersection (){
 Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
+double b1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
- 
- 
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
- 
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
- 
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+double b2 = Convert.ToDouble(Console.ReadLine());
 
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+}
 
 // Задача 44:выведите первые N чисел
 // Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
