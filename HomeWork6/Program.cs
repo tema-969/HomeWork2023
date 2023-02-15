b8a0e289c988c25ee3d3886960cb14be400013d3
Шумилов Артем Михайлович <tema-969@yandex.ru>
2023-02-15 21:47:44 +0300

    Решено 1 задание

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -2,6 +2,23 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+Console.WriteLine("Введите числа через пробел: ");
+string strnumber = Console.ReadLine();
+CountNumber(strnumber);
+
+void CountNumber (string strnumbers ){
+string[] strnum = strnumber.Split(' ');
+int count = 0;
+foreach (var item in strnum)
+{
+    int num = Convert.ToInt32(item);
+    if (num >0) {
+        count++;
+    }
+}
+System.Console.WriteLine("Количество чисел больше 0 = "+count);
+}
+
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
