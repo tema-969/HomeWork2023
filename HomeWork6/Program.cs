72bc369d73047e0df5a26a0a526e18a308477721
Шумилов Артем Михайлович <tema-969@yandex.ru>
2023-02-19 20:15:16 +0300

    Начинаю решение ДЗ

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -3,7 +3,7 @@
 // 1, -7, 567, 89, 223-> 3
 
 
-//CountNumber();
+CountNumber();
 
 void CountNumber (){
 Console.WriteLine("Введите числа через пробел: ");
@@ -22,7 +22,7 @@ System.Console.WriteLine("Количество чисел больше 0 = "+cou
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-//Intersection();
+Intersection();
 
 void Intersection (){
 Console.Write("Введите k1: ");
